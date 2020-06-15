761376df98b8d620e9b67c4bc46ca4880feb0ead
Jordan <jordanleelong5@gmail.com>
2020-06-14 20:36:50 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Your code here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office") {
+return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(string="*") {
+
+
+    return function(param="special") {
+        return `You are ${string}${param}${string}!`
+    }
+}
+
+wrapAdjective("||")("a dedicated programmer||!")
+
+let Calculator = {
+    add: function() {
+        return 1 + 3
+    },
+    subtract: function() {
+        return 1 - 3
+    },
+
+    multiply: function() {
+        return 1 * 3
+    },
+
+    divide: function() {
+        return 10 / 5
+    }
+    
+}
+
+function actionApplyer(start, ray) {
+    let a = start
+
+    for (let i = 0; i < ray.length; i++ ){
+      a = ray[i](a)
+    }
+  
+    return a
+
+
+}
+ 
