f7707800ef653cf848724b979406353c5305c119
antony_sanders <antony.devon92@gmail.com>
2020-06-16 02:08:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+};
+
+saturdayFun("bathe my dog");
+
+let mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+};
+
+mondayWork("work from home");
+
+function wrapAdjective(flair = "*") {
+    return function(adj = "special") {
+        return `You are ${flair}${adj}${flair}!`
+    }
+}
+
+let Calculator = {
+
+}
