6950aaa55b210f6e3277f180ebf3dca1f5b5bb54
Mug <brad.alexander913@gmail.com>
2020-07-03 15:32:46 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+function saturdayFun(act="roller-skate") {
+return "This Saturday, I want to "+act+"!"
+}
+let mondayWork = function(act="go to the office"){
+    return "This Monday, I will "+ act+"."
+}
+
+let wrapAdjective = function (flair="*"){
+    return function(param="special"){
+        return `You are ${flair}${param}${flair}!`
+
+    }
+}
+
+let Calculator = {
+    add: function(num1,num2){
+        return num1 + num2
+    },
+    subtract: function(num1,num2){
+        return num1 - num2
+    },
+    multiply: function(num1,num2){
+        return num1 * num2
+    },
+    divide: function(num1,num2){
+        return num1 / num2
+    }
+}
+function actionApplyer(int,arr){
+    
+    for (let i=0; i< arr.length; i++){
+        int = arr[i](int)
+    }
+    
+    return int
+
+}
