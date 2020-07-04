6e1656281f483a42120b3f676c1e010597eb1a32
Wills Blake <richwblake@gmail.com>
2020-07-03 22:37:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+
+// First Challenge
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+// Second Challenge
+function mondayWork (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+// Third Challenge
+function wrapAdjective (flair = "*") {
+    return (desc = "special") => `You are ${flair}${desc}${flair}!`
+}
+
+const Calculator = {
+    add: () => 1 + 3,
+    subtract: () => 1 - 3,
+    multiply: () => 1 * 3,
+    divide: () => 10 / 5
+};
+
+function actionApplyer (integer, arrayOfFunctions) {
+    if (arrayOfFunctions.length === 0) {
+        return integer;
+    } else {
+         arrayOfFunctions.forEach(func => integer = func(integer));
+         return integer;
+    }
+}
